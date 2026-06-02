7faebf09e4a3f9089c61520c703c8eccd1c8a2b7
Phoenix Bogati <phoenix.bogati@gmail.com>
2026-06-02 00:09:24 -0700

    v0.2.1: made main.py and shelf_config more cleaner

--- src/main.py
+++ src/main.py
@@ -2,29 +2,17 @@ import cv2
 
 from camera import open_camera, read_frame, release_camera
 
-from shelf_config import SHELF_SLOTS
+from shelf_config import *
 
 
 def main():
     camera = open_camera(camera_index=0)
-    print("Camera Opened Successfully\n\n")
+
     while True:
         frame= read_frame(camera)
-        for slots in SHELF_SLOTS:
-            x= slots['x']
-            y=slots['y']
-            width = slots['width']
-            height= slots['height']
+        draw_shelf_slots(frame)
 
-            top_left = (x,y)
-            bottom_right = (x+ width, y + height)
-
-            cv2.rectangle(frame, top_left,bottom_right,(0,255,0), 2)
-
-
-        #print(f"frame shape: {frame.shape}")
-
-        cv2.imshow("Testing Frames: ", frame)
+        cv2.imshow("Smart Shelf Monitor: ", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     
@@ -33,11 +21,6 @@ def main():
 
 
     
-
-
-
-
-
 
 if __name__ == "__main__":
     main()
--- src/shelf_config.py
+++ src/shelf_config.py
@@ -1,12 +1,25 @@
-SHELF_SLOTS = []
+import cv2
 
-SHELF_SLOTS= [
+shelf_slots= [
     { 'name': 'A1',
-     'x': 00,
-     'y': 00,
+     'x': 0,
+     'y': 0,
      'width': 200,
-     'height': 150     
+     'height': 200     
      
      }
 ]
 
+
+
+def draw_shelf_slots(frame):
+    for slots in shelf_slots:
+        x1, y1, x2, y2 = slots['x'], slots['y'], slots['x']+slots['width'], slots['y']+ slots['height']
+
+        cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0),3)
+
+        cv2.putText(frame, slots['name'], (x1, y1 + 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
+        return frame
+    
+    
