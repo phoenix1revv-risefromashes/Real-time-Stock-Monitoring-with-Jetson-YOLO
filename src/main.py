44a0aefb788cbafcfee75649dd3a8dbda2df5ef5
Phoenix Bogati <phoenix.bogati@gmail.com>
2026-06-01 23:06:12 -0700

    v0.2: add shelf region Mapping feature

--- src/main.py
+++ src/main.py
@@ -2,11 +2,26 @@ import cv2
 
 from camera import open_camera, read_frame, release_camera
 
+from shelf_config import SHELF_SLOTS
+
+
 def main():
     camera = open_camera(camera_index=0)
     print("Camera Opened Successfully\n\n")
     while True:
         frame= read_frame(camera)
+        for slots in SHELF_SLOTS:
+            x= slots['x']
+            y=slots['y']
+            width = slots['width']
+            height= slots['height']
+
+            top_left = (x,y)
+            bottom_right = (x+ width, y + height)
+
+            cv2.rectangle(frame, top_left,bottom_right,(0,255,0), 2)
+
+
         #print(f"frame shape: {frame.shape}")
 
         cv2.imshow("Testing Frames: ", frame)
---
+++ src/shelf_config.py
@@ -0,0 +1,12 @@
+SHELF_SLOTS = []
+
+SHELF_SLOTS= [
+    { 'name': 'A1',
+     'x': 00,
+     'y': 00,
+     'width': 200,
+     'height': 150     
+     
+     }
+]
+
