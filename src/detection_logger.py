68f19bd84c84c7ee9351d35dde17002c0553f1d7
Phoenix Bogati <phoenix.bogati@gmail.com>
2026-06-04 16:52:37 -0700

    created a detection_logger.py module that handles detection_logs.csv

---
+++ src/detection_logger.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from pathlib import Path
+import csv
+
+
+log_path = Path('data/logs/detection_log.csv')
+
+def initialize_log_file(log_path = log_path):
+    log_path.parent.mkdir(parents=True, exist_ok=True)
+
+    if not log_path.exists():
+        with open(log_path, mode='w', newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["timestamp", "slot_name", "status", "edge_pixels", "threshold"])
+
+def log_detection(slot_name, status, edge_pixels, threshold, log_path=log_path):
+    timestamp = datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
+
+    with open (log_path, mode='a', newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([timestamp, slot_name, status, edge_pixels, threshold])
+
